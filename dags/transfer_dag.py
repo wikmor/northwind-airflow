99c5cf54c2becf2e7fee0a8869f2e66c507bec59
wikmor <wikmor@proton.me>
2024-06-09 14:16:38 +0200

    Refactor transfer dag

--- dags/transfer_dag.py
+++ dags/transfer_dag.py
@@ -21,91 +21,120 @@ dag = DAG(
 )
 
 
-def transfer_data():
+def to_customer_dimension():
+    source_sql = "SELECT customer_id, company_name, city, country FROM customers;"
+
+    destination_sql = "INSERT INTO customer (customer_id, company_name, city, country) VALUES (%s, %s, %s, %s)"
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def to_supplier_dimension():
+    source_sql = "SELECT supplier_id, company_name, country FROM suppliers;"
+
+    destination_sql = "INSERT INTO supplier (supplier_id, company_name, country) VALUES (%s, %s, %s)"
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def to_time_dimension():
+    source_sql = """
+    SELECT DISTINCT 
+    order_id AS time_id, 
+    order_date, 
+    date_part('year', order_date) AS year, 
+    date_part('quarter', order_date) AS quarter, date_part('month', order_date) AS month, 
+    date_part('day', order_date) AS day 
+    FROM orders;
+    """
+
+    destination_sql = "INSERT INTO time (time_id, date, year, quarter, month, day) VALUES (%s, %s, %s, %s, %s, %s)"
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def to_product_dimension():
+    source_sql = """
+    SELECT product_id, product_name, category_name 
+    FROM products 
+    JOIN categories c ON c.category_id = products.category_id;
+    """
+
+    destination_sql = "INSERT INTO product (product_id, product_name, product_category) VALUES (%s, %s, %s)"
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def to_employee_dimension():
+    source_sql = """
+    SELECT emp.employee_id, emp.last_name AS emp_last_name, mng.last_name AS mng_last_name 
+    FROM employees emp 
+    LEFT JOIN employees mng on emp.reports_to = mng.employee_id;
+    """
+
+    destination_sql = "INSERT INTO employee (employee_id, emp_last_name, mng_last_name) VALUES (%s, %s, %s)"
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def to_order_facts():
+    source_sql = """
+    SELECT customer_id, od.product_id, employee_id, od.order_id, supplier_id, od.unit_price, quantity 
+    FROM order_details od 
+    JOIN orders o ON od.order_id = o.order_id 
+    JOIN products p ON od.product_id = p.product_id;
+    """
+
+    destination_sql = """
+    INSERT INTO orders_facts (customer_id, product_id, employee_id, time_id, supplier_id, price, quantity) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """
+
+    _transfer_records(source_sql, destination_sql)
+
+
+def _transfer_records(source_sql, destination_sql):
     source_hook = PostgresHook(postgres_conn_id='postgres_source')
     star_hook = PostgresHook(postgres_conn_id='postgres_star')
 
-    # SQL to fetch data from source database
-    source_sql = """
-                    SELECT emp.employee_id, emp.last_name AS emp_last_name, mng.last_name AS mng_last_name
-                    FROM employees emp LEFT JOIN employees mng on emp.reports_to = mng.employee_id;
-                 """
     source_data = source_hook.get_records(source_sql)
-
-    destination_sql = "INSERT INTO employee (employee_id, emp_last_name, mng_last_name) VALUES (%s, %s, %s)"
-    # SQL to insert data into destination database
-    if source_data:
-        for row in source_data:
-            star_hook.run(destination_sql, parameters=row)
-
-    # SQL to fetch data from source database
-    source_sql = "SELECT supplier_id, company_name, country FROM suppliers;"
-    source_data = source_hook.get_records(source_sql)
-
-    destination_sql = "INSERT INTO supplier (supplier_id, company_name, country) VALUES (%s, %s, %s)"
-    # SQL to insert data into destination database
-    if source_data:
-        for row in source_data:
-            star_hook.run(destination_sql, parameters=row)
-
-    # SQL to fetch data from source database
-    source_sql = "SELECT customer_id, company_name, city, country FROM customers;"
-    source_data = source_hook.get_records(source_sql)
-
-    destination_sql = "INSERT INTO customer (customer_id, company_name, city, country) VALUES (%s, %s, %s, %s)"
-    # SQL to insert data into destination database
-    if source_data:
-        for row in source_data:
-            star_hook.run(destination_sql, parameters=row)
-
-    source_sql = """SELECT DISTINCT 
-        order_id AS time_id, 
-        order_date, 
-        date_part('year', order_date) AS year, 
-        date_part('quarter', order_date) AS quarter, 
-        date_part('month', order_date) AS month, 
-        date_part('day', order_date) AS day 
-    FROM orders;"""
-    source_data = source_hook.get_records(source_sql)
-
-    destination_sql = "INSERT INTO time (time_id, date, year, quarter, month, day) VALUES (%s, %s, %s, %s, %s, %s)"
-    # SQL to insert data into destination database
     if source_data:
         for row in source_data:
             star_hook.run(destination_sql, parameters=row)
 
 
-    source_sql = """
-                    SELECT product_id, product_name, category_name
-                    FROM products 
-                    JOIN categories c ON c.category_id = products.category_id;
-                 """
-    source_data = source_hook.get_records(source_sql)
+customer_task = PythonOperator(
+    task_id='to_customer_dimension',
+    python_callable=to_customer_dimension,
+    dag=dag
+)
 
-    destination_sql = "INSERT INTO product (product_id, product_name, product_category) VALUES (%s, %s, %s)"
-    # SQL to insert data into destination database
-    if source_data:
-        for row in source_data:
-            star_hook.run(destination_sql, parameters=row)
+supplier_task = PythonOperator(
+    task_id='to_supplier_dimension',
+    python_callable=to_supplier_dimension,
+    dag=dag
+)
 
-    source_sql = """
-                    SELECT customer_id, od.product_id, employee_id, od.order_id, supplier_id, od.unit_price, quantity
-                    FROM order_details od 
-                    JOIN orders o on od.order_id = o.order_id
-                    JOIN products p on od.product_id = p.product_id;
-                 """
-    source_data = source_hook.get_records(source_sql)
+time_task = PythonOperator(
+    task_id='to_time_dimension',
+    python_callable=to_time_dimension,
+    dag=dag
+)
 
-    destination_sql = "INSERT INTO orders_facts (customer_id, product_id, employee_id, time_id, supplier_id, price, quantity) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    # SQL to insert data into destination database
-    if source_data:
-        for row in source_data:
-            star_hook.run(destination_sql, parameters=row)
+product_task = PythonOperator(
+    task_id='to_product_dimension',
+    python_callable=to_product_dimension,
+    dag=dag
+)
 
+employee_task = PythonOperator(
+    task_id='to_employee_dimension',
+    python_callable=to_employee_dimension,
+    dag=dag
+)
 
-
-transfer_task = PythonOperator(
-    task_id='transfer_data',
-    python_callable=transfer_data,
+order_task = PythonOperator(
+    task_id='to_order_facts',
+    python_callable=to_order_facts,
     dag=dag
 )
