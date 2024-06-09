43fa906536ddd47d0bb5942f933d2aade7ed7887
wikmor <wikmor@proton.me>
2024-06-09 18:25:59 +0200

    Refactor code

--- dags/new_data_simulator_dag.py
+++ dags/new_data_simulator_dag.py
@@ -26,43 +26,33 @@ dag = DAG(
     catchup=False
 )
 
-data = [
-    (fake.company(), None, None, None, None, None, None, None, None, None, None),
-    (fake.company(), None, None, None, None, None, None, fake.country(), None, None, None),
-    (fake.company(), None, None, None, None, None, None, fake.country(), None, None, None),
-]
 
-values_sql = ', '.join(['(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'] * len(data))
+def generate_suppliers_data():
+    suppliers = []
+    for _ in range(random.randint(1, 5)):
+        rand_row = [fake.company(), None, None, None, None, None, None, _generate_nullable_country(), None, None, None]
+        suppliers.append(rand_row)
+    return suppliers
 
-insert_rows = PostgresOperator(
-    task_id="insert_rows",
+
+def _generate_nullable_country():
+    if random.random() < 0.2:
+        return None
+    else:
+        return fake.country()
+
+
+suppliers_data = generate_suppliers_data()
+
+suppliers_values_sql = ', '.join(['(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'] * len(suppliers_data))
+
+insert_suppliers = PostgresOperator(
+    task_id="insert_suppliers",
     postgres_conn_id="postgres_source",
     sql=f"""
     INSERT INTO suppliers (company_name, contact_name, contact_title, address, city, region, postal_code, country, phone, fax, homepage) 
-    VALUES {values_sql}
+    VALUES {suppliers_values_sql}
     """,
-    parameters=[item for sublist in data for item in sublist],
+    parameters=[item for sublist in suppliers_data for item in sublist],
     dag=dag
 )
-
-
-# def generate_new_data():
-#     source_hook = PostgresHook(postgres_conn_id='postgres_source')
-#     company_name1, country1 = generate_suppliers()
-#     insert_sql = """
-#     INSERT INTO suppliers VALUES (company_name1, NULL, NULL, NULL, NULL, NULL, NULL, country1, NULL, NULL, NULL);
-#     """
-#     source_hook.run(insert_sql)
-
-
-# def generate_suppliers():
-#     company_name = fake.company()
-#     country = _generate_nullable_country()
-#     return company_name, country
-#
-#
-# def _generate_nullable_country():
-#     if random.random() < 0.2:
-#         return None
-#     else:
-#         return fake.country()
