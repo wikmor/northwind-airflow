aac52f4fa2daa9b2a3ee485a446c5649f9ce00fd
wikmor <wikmor@proton.me>
2024-06-09 18:46:40 +0200

    Remove unused import

--- dags/new_data_simulator_dag.py
+++ dags/new_data_simulator_dag.py
@@ -1,10 +1,8 @@
 import random
 
 from airflow import DAG
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
-
 from faker import Faker
 
 fake = Faker()
